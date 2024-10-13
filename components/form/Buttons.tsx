5b6162826aa091d21714e41f0d382cadb0aa2bc7
dafantr <dafantara@gmail.com>
2024-10-13 22:18:32 +0700

    changes until favorites page

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -3,6 +3,8 @@
 import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
+import { SignInButton } from '@clerk/nextjs';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
 
 type btnSize = 'default' | 'lg' | 'sm';
 
@@ -13,26 +15,62 @@ type SubmitButtonProps = {
 };
 
 export function SubmitButton({
-    className = '',
-    text = 'submit',
-    size = 'lg',
-  }: SubmitButtonProps) {
-    const { pending } = useFormStatus();
-    return (
+  className = '',
+  text = 'submit',
+  size = 'lg',
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type='submit'
+      disabled={pending}
+      className={`capitalize ${className}`}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode='modal'>
       <Button
-        type='submit'
-        disabled={pending}
-        className={`capitalize ${className}`}
-        size={size}
+        type='button'
+        size='icon'
+        variant='outline'
+        className='p-2 cursor-pointer'
+        asChild
       >
-        {pending ? (
-          <>
-            <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
-            Please wait...
-          </>
-        ) : (
-          text
-        )}
+        <FaRegHeart />
       </Button>
-    );
-  }
+    </SignInButton>
+  );
+};
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='outline'
+      className=' p-2 cursor-pointer'
+    >
+      {pending ? (
+        <ReloadIcon className=' animate-spin' />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
+  );
+};
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,42 @@
-import { Input } from "../ui/input"
+'use client';
+import { Input } from '../ui/input';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { useState, useEffect } from 'react';
 
 function NavSearch() {
-  return (
-    <Input 
-    type="text" 
-    placeholder="find a property..." 
-    className="max-w-xs dark:bg-muted"/>
-  )
-}
+  const searchParams = useSearchParams();
 
-export default NavSearch
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get('search')?.toString() || ''
+  );
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set('search', value);
+    } else {
+      params.delete('search');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+  useEffect(() => {
+    if (!searchParams.get('search')) {
+      setSearch('');
+    }
+  }, [searchParams.get('search')]);
+  return (
+    <Input
+      type='search'
+      placeholder='Find a property...'
+      className='max-w-xs dark:bg-muted '
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
+    />
+  );
+}
+export default NavSearch;
